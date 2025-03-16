c19880b05278debdab8150fe61adba3d25dc2e9c
naseh42 <naseh42@gmail.com>
2025-03-17 00:36:50 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+// تنظیمات پایگاه‌داده
+$host = "localhost";
+$username = "root";
+$password = "naseh42";
+$dbname = "vpn_users";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("خطای اتصال به پایگاه‌داده: " . $conn->connect_error);
+}
+
+// تنظیم زبان
+$lang = isset($_GET['lang']) && $_GET['lang'] == 'en' ? 'en' : 'fa';
+$translations = [
+    'fa' => [
+        'welcome' => 'به پنل مدیریت خوش آمدید',
+        'add_user' => 'افزودن کاربر',
+        'manage_servers' => 'مدیریت سرورها',
+        'server_info' => 'مشخصات سرور',
+    ],
+    'en' => [
+        'welcome' => 'Welcome to the Admin Panel',
+        'add_user' => 'Add User',
+        'manage_servers' => 'Manage Servers',
+        'server_info' => 'Server Info',
+    ]
+];
+$current_translations = $translations[$lang];
+?>
